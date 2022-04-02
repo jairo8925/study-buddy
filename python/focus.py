11a0187ff1efe0e3bd142d0b16e457d6fd9c07fa
Tanay <tanaycv@cs.washington.edu>
2022-04-02 14:39:59 -0700

    focus finished

--- python/focus.py
+++ python/focus.py
@@ -1,26 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Estimate Relaxation from Band Powers
-
-This example shows how to buffer, epoch, and transform EEG data from a single
-electrode into values for each of the classic frequencies (e.g. alpha, beta, theta)
-Furthermore, it shows how ratios of the band powers can be used to estimate
-mental state for neurofeedback.
-
-The neurofeedback protocols described here are inspired by
-*Neurofeedback: A Comprehensive Review on System Design, Methodology and Clinical Applications* by Marzbani et. al
-
-Adapted from https://github.com/NeuroTechX/bci-workshop
-"""
-
-# import numpy as np  # Module that simplifies computations on matrices
-# import matplotlib.pyplot as plt  # Module used for plotting
-# from pylsl import StreamInlet, resolve_byprop  # Module to receive EEG data
-# import utils  # Our own utility functions
-# from blinkFilter import filt
-# from scipy import signal
-# import pyautogui # for spacebar
 from flask import Flask
+from copyreg import pickle
+import numpy as np  # Module that simplifies computations on matrices
+import matplotlib.pyplot as plt  # Module used for plotting
+from pylsl import StreamInlet, resolve_byprop  # Module to receive EEG data
+
+import bci_workshop_tools as BCIw  # Our own functions for the workshop
+from pickle import dump, load
+import joblib
+model_fileName = "model.pkl"
+mu_fileName = "mu.pkl"
+std_fileName = "std.pkl"
+classifier = joblib.load(model_fileName)
+mu_ft = load(open(mu_fileName, 'rb'))
+std_ft = load(open(std_fileName, 'rb'))
 
 # Flask Setup
 app = Flask(__name__)
@@ -30,118 +22,106 @@ def get_focus():
 
 focus = True
 
-# # Handy little enum to make code more readable
+""" 1. CONNECT TO EEG STREAM """
+# Search for active LSL stream
+print('Looking for an EEG stream...')
+streams = resolve_byprop('type', 'EEG', timeout=2)
+if len(streams) == 0:
+    raise RuntimeError('Can\'t find EEG stream.')
 
+# Set active EEG stream to inlet and apply time correction
+print("Start acquiring data")
+inlet = StreamInlet(streams[0], max_chunklen=12)
+eeg_time_correction = inlet.time_correction()
 
+# Get the stream info, description, sampling frequency, number of channels
+info = inlet.info()
+description = info.desc()
+fs = int(info.nominal_srate())
+n_channels = info.channel_count()
 
+# Get names of all channels
+ch = description.child('channels').first_child()
+ch_names = [ch.child_value('label')]
+for i in range(1, n_channels):
+    ch = ch.next_sibling()
+    ch_names.append(ch.child_value('label'))
 
-# class Band:
-#     Delta = 0
-#     Theta = 1
-#     Alpha = 2
-#     Beta = 3
+ # Length of the EEG data buffer (in seconds)
+    # This buffer will hold last n seconds of data and be used for calculations
+buffer_length = 15
 
+    # Length of the epochs used to compute the FFT (in seconds)
+epoch_length = 1
 
-# """ EXPERIMENTAL PARAMETERS """
-# # Modify these to change aspects of the signal processing
+    # Amount of overlap between two consecutive epochs (in seconds)
+overlap_length = 0.8
 
-# # Length of the EEG data buffer (in seconds)
-# # This buffer will hold last n seconds of data and be used for calculations
-# BUFFER_LENGTH = 5
+    # Amount to 'shift' the start of each next consecutive epoch
+shift_length = epoch_length - overlap_length
 
-# # Length of the epochs used to compute the FFT (in seconds)
-# EPOCH_LENGTH = 0.2
+    # Index of the channel (electrode) to be used
+    # 0 = left ear, 1 = left forehead, 2 = right forehead, 3 = right ear
+index_channel = [0, 1, 2, 3]
+    # Name of our channel for plotting purposes
+ch_names = [ch_names[i] for i in index_channel]
+n_channels = len(index_channel)
 
-# # Amount of overlap between two consecutive epochs (in seconds)
-# OVERLAP_LENGTH = 0.1
+    # Get names of features
+# ex. ['delta - CH1', 'pwr-theta - CH1', 'pwr-alpha - CH1',...]
+feature_names = BCIw.get_feature_names(ch_names)
 
-# # Amount to 'shift' the start of each next consecutive epoch
-# SHIFT_LENGTH = EPOCH_LENGTH - OVERLAP_LENGTH
+# Number of seconds to collect training data for (one class)
+training_length = 20
+  # Initialize the buffers for storing raw EEG and decisions
+eeg_buffer = np.zeros((int(fs * buffer_length), n_channels))
+filter_state = None  # for use with the notch filter
+decision_buffer = np.zeros((30, 1))
 
-# # Index of the channel(s) (electrodes) to be used
-# # 0 = left ear, 1 = left forehead, 2 = right forehead, 3 = right ear
-# INDEX_CHANNEL = [0]
+plotter_decision = BCIw.DataPlotter(30, ['Decision'])
 
-# if __name__ == "__main__":
+    # The try/except structure allows to quit the while loop by aborting the
+    # script with <Ctrl-C>
+print('Press Ctrl-C in the console to break the while loop.')
 
-#     """ 1. CONNECT TO EEG STREAM """
+# try:
+while True:
 
-#     # Search for active LSL streams
-#     print('Looking for an EEG stream...')
-#     streams = resolve_byprop('type', 'EEG', timeout=2)
-#     if len(streams) == 0:
-#         raise RuntimeError('Can\'t find EEG stream.')
+    """ 3.1 ACQUIRE DATA """
+    # Obtain EEG data from the LSL stream
+    eeg_data, timestamp = inlet.pull_chunk(
+            timeout=1, max_samples=int(shift_length * fs))
 
-#     # Set active EEG stream to inlet and apply time correction
-#     print("Start acquiring data")
-#     inlet = StreamInlet(streams[0], max_chunklen=12)
-#     eeg_time_correction = inlet.time_correction()
+    # Only keep the channel we're interested in
+    ch_data = np.array(eeg_data)[:, index_channel]
 
-#     # Get the stream info and description
-#     info = inlet.info()
-#     description = info.desc()
+    # Update EEG buffer
+    eeg_buffer, filter_state = BCIw.update_buffer(
+            eeg_buffer, ch_data, notch=True,
+            filter_state=filter_state)
 
-#     # Get the sampling frequency
-#     # This is an important value that represents how many EEG data points are
-#     # collected in a second. This influences our frequency band calculation.
-#     # for the Muse 2016, this should always be 256
-#     fs = int(info.nominal_srate())
+    """ 3.2 COMPUTE FEATURES AND CLASSIFY """
+    # Get newest samples from the buffer
+    data_epoch = BCIw.get_last_data(eeg_buffer,
+                                    epoch_length * fs)
 
-#     """ 2. INITIALIZE BUFFERS """
+    # Compute features
+    feat_vector = BCIw.compute_feature_vector(data_epoch, fs)
+    y_hat = BCIw.test_classifier(classifier,
+                                    feat_vector.reshape(1, -1), mu_ft,
+                                    std_ft)
+                                    
+    focus = bool(y_hat)
+    
+    # print(y_hat) #prints 1 or 0 based on decision
 
-#     # Initialize raw EEG data buffer
-#     eeg_buffer = np.zeros((int(fs * BUFFER_LENGTH), 1))
-#     filter_state = None  # for use with the notch filter
+    # decision_buffer, _ = BCIw.update_buffer(decision_buffer,
+    #                                         np.reshape(y_hat, (-1, 1)))
 
-#     # Compute the number of epochs in "buffer_length"
-#     n_win_test = int(np.floor((BUFFER_LENGTH - EPOCH_LENGTH) /
-#                               SHIFT_LENGTH + 1))
+            # """ 3.3 VISUALIZE THE DECISIONS """
+            # plotter_decision.update_plot(decision_buffer)
+            # plt.pause(0.00001)
 
-#     # Initialize the band power buffer (for plotting)
-#     # bands will be ordered: [delta, theta, alpha, beta]
-#     band_buffer = np.zeros((n_win_test, 4))
+# except KeyboardInterrupt:
 
-#     """ 3. GET DATA """
-
-#     # The try/except structure allows to quit the while loop by aborting the
-#     # script with <Ctrl-C>
-#     print('Press Ctrl-C in the console to break the while loop.')
-
-#     try:
-#         # The following loop acquires data, computes band powers, and calculates neurofeedback metrics based on those band powers
-#         while True:
-
-#             """ 3.1 ACQUIRE DATA """
-#             # Obtain EEG data from the LSL stream
-#             eeg_data, timestamp = inlet.pull_chunk(
-#                 timeout=1, max_samples=int(SHIFT_LENGTH * fs))
-
-#             # Only keep the channel we're interested in
-#             ch_data = np.array(eeg_data)[:, INDEX_CHANNEL]
-
-#             # Update EEG buffer with the new data
-#             eeg_buffer, filter_state = utils.update_buffer(
-#                 eeg_buffer, ch_data, notch=True,
-#                 filter_state=filter_state)
-
-#             """ 3.2 COMPUTE BAND POWERS """
-#             # Get newest samples from the buffer
-#             data_epoch = utils.get_last_data(eeg_buffer,
-#                                              int(EPOCH_LENGTH * fs))
-#             #print(data_epoch.shape)
-
-#             matchFilt = signal.hilbert(filt)
-
-#             matches = signal.correlate(matchFilt,data_epoch[:,0])
-
-#             matchesAbs = np.abs(matches[:])
-
-#             maxMatch = np.max(matchesAbs)/1e5
-#             print(maxMatch)
-
-#             if maxMatch > 4.5:
-#                 print('blink')  
-#                 pyautogui.press("space")
-     
-#     except KeyboardInterrupt:
-#         print('Closing!')
+#         print('Closed!')
