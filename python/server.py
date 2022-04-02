bbb09f35608bb61add1de69e9bc2da13bf58adef
jairo8925 <csarmiento2333@gmail.com>
2022-04-02 13:54:47 -0700

    fix cors error

--- python/server.py
+++ python/server.py
@@ -1,8 +1,12 @@
+import re
 from flask import Flask
+from flask import jsonify
 from focus import get_focus
 
 app = Flask(__name__)
 
-@app.route('/focus')
+@app.route('/focus', methods=['GET'])
 def focus_state():
-    return { "state": get_focus() }
+    response = jsonify({'state': get_focus()})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
