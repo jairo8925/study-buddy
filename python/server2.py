4aae13f2a5a1b7650973b7500c73693b86bcb3e2
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2022-04-03 12:26:52 -0700

    Update server2.py

--- python/server2.py
+++ python/server2.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def start_focus():
     connect_and_use()
 
-@app.route('/focus', methods=['GET'])
+@app.route('/get_focus', methods=['GET'])
 def get_focus():
     response = jsonify({'state': data["focus"]})
     response.headers.add('Access-Control-Allow-Origin', '*')
