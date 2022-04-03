f3b49a22e010a9896559fb656a2160ec766db0cc
jairo8925 <csarmiento2333@gmail.com>
2022-04-03 01:04:38 -0700

    added binaural beats

--- frontend/src/components/Study.js
+++ frontend/src/components/Study.js
@@ -1,12 +1,14 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import Task from './Task';
-import Image from 'react-bootstrap/Image';
+import { Button, Image } from 'react-bootstrap';
 import muse from '../apis/muse';
 import './Study.css';
 
 const Study = ({ task, duration, onFinish }) => {
   // Data we get from the muse (Focused or not focused)
   const [focused, setFocused] = useState(false);
+  const [paused, setPaused] = useState(true);
+  const beats = useRef(new Audio('beats.mp3'));
 
   const callMuse = useCallback(async () => {
     const response = await muse.get('/focus');
@@ -25,10 +27,15 @@ const Study = ({ task, duration, onFinish }) => {
     onFinish();
   };
 
+  const onBeatsClick = () => {
+    setPaused(!paused);
+    paused ? beats.current.play() : beats.current.pause();
+  };
+
   return (
     <div style={{ marginTop: '35px' }}>
       <h4 className="text-center">
-        You are currently{' '}
+        you are currently{' '}
         <b className={focused ? 'focused' : 'distracted'}>
           {focused ? 'focused' : 'not focused'}
         </b>
@@ -38,9 +45,19 @@ const Study = ({ task, duration, onFinish }) => {
           src="./lofiMuse.jpg"
           className="fluid rounded shadow-2-strong"
           alt="LoFi Muse"
-          style={{ maxWidth: '27rem', marginBottom: '13px', marginTop: '8px' }}
+          style={{
+            maxWidth: '27rem',
+            marginBottom: '13px',
+            marginTop: '8px',
+          }}
         ></Image>
         <Task task={task} duration={duration} onTaskFinish={onTaskFinish} />
+        <Button
+          variant={paused ? 'outline-secondary' : 'secondary'}
+          onClick={onBeatsClick}
+        >
+          {paused ? 'get focused' : 'stop the sound'}
+        </Button>
       </div>
     </div>
   );
